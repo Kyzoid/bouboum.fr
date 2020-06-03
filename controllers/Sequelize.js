30a1f2c6b8125ca002b8ceeccf6367b2b5a7400c
Kyzoid <kyzoid@gmail.com>
2020-06-03 17:06:19 +0200

    Add front info message on editor page

--- controllers/Sequelize.js
+++ controllers/Sequelize.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const Sequelize = require("sequelize");
-const { DATABASE_URL, IN_PROD } = require('../config')
+const { DATABASE_URL } = require('../config')
 
-const connection = new Sequelize(DATABASE_URL, { logging: !IN_PROD });
+const connection = new Sequelize(DATABASE_URL, { logging: false });
 connection
   .authenticate()
   .then(() => console.log("connected to PG"))
