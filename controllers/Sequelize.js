9af0226ac1a7003e5cdcdfa680c0cacb8031c720
Kyzoid <kyzoid@gmail.com>
2020-06-03 14:38:36 +0200

    Remove Sequelize logs when env equals production

--- controllers/Sequelize.js
+++ controllers/Sequelize.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const Sequelize = require("sequelize");
-const { DATABASE_URL } = require('../config')
+const { DATABASE_URL, IN_PROD } = require('../config')
 
-const connection = new Sequelize(DATABASE_URL);
+const connection = new Sequelize(DATABASE_URL, { logging: !IN_PROD });
 connection
   .authenticate()
   .then(() => console.log("connected to PG"))
