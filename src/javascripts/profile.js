257d1dccd3aa4f797dd7080c785c8a119973e825
Kyzoid <kyzoid@gmail.com>
2020-08-06 17:26:33 +0200

    Add multiple chartist graph

--- src/javascripts/profile.js
+++ src/javascripts/profile.js
@@ -1,37 +1,71 @@
 const playerId = document.getElementById('player').dataset.playerId;
 
 const getBouboumRankings = async () => {
-    return await fetch(`/profil/${playerId}/bouboum`, { method: 'GET' }).then((res) => res.json());
+  return await fetch(`/profil/${playerId}/bouboum`, { method: 'GET' }).then((res) => res.json());
 };
 
 const getAaaahRankings = async () => {
-    return await fetch(`/profil/${playerId}/aaaah`, { method: 'GET' }).then((res) => res.json());
+  return await fetch(`/profil/${playerId}/aaaah`, { method: 'GET' }).then((res) => res.json());
 };
 
-const chartPoints = [];
-const data = {
+const formattedBouboumPlayersDataForChartist = (bouboumRankings) => {
+  return {
+    mode: 'Bouboum',
+    win: bouboumRankings.map(bouboumRanking => bouboumRanking.win),
+    ratio: bouboumRankings.map(bouboumRanking => bouboumRanking.ratio),
+    total: bouboumRankings.map(bouboumRanking => bouboumRanking.total),
+    winRatio: bouboumRankings.map(bouboumRanking => ((bouboumRanking.win/bouboumRanking.total)*100).toFixed(2)),
+  }
+};
+
+const formattedAaaahPlayersDataForChartist = (aaaahRankings) => {
+  return {
+    mode: 'Aaaah',
+    win: aaaahRankings.map(aaaahRanking => aaaahRanking.win),
+    winRatio: aaaahRankings.map(aaaahRanking => aaaahRanking.win_ratio/100),
+    guiding: aaaahRankings.map(aaaahRanking => aaaahRanking.guiding),
+    kill: aaaahRankings.map(aaaahRanking => aaaahRanking.kill),
+    guidingRatio: aaaahRankings.map(aaaahRanking => aaaahRanking.guiding_ratio),
+    total: aaaahRankings.map(aaaahRanking => aaaahRanking.total)
+  }
+};
+
+const drawChart = (rankings, key) => {
+  const data = {
     labels: [],
     series: [
-        chartPoints
+      rankings[key],
     ]
-};
+  };
 
-const options = {
+  const options = {
     width: 700,
     height: 300
+  };
+
+  new Chartist.Line('.ct-chart', data, options);
 };
 
+let bouboumData = null;
+let aaaahData = null;
+
 getBouboumRankings().then(async (bouboumRankings) => {
-    if (bouboumRankings.length) {
-        bouboumRankings.forEach(bouboumRanking => {
-            chartPoints.push(bouboumRanking.win);
-        });
-    } else {
-        const aaaahRankings = await getAaaahRankings();
-        aaaahRankings.forEach(aaaahRanking => {
-            chartPoints.push(aaaahRanking.win);
-        });
+  if (bouboumRankings.length) {
+    bouboumData = formattedBouboumPlayersDataForChartist(bouboumRankings);
+    drawChart(formattedBouboumPlayersDataForChartist(bouboumRankings), 'win');
+  } else {
+    const aaaahRankings = await getAaaahRankings();
+    aaaahData = formattedAaaahPlayersDataForChartist(aaaahRankings);
+    drawChart(formattedAaaahPlayersDataForChartist(aaaahRankings), 'win');
+  }
+});
+
+const chartistButtons = document.getElementsByClassName('chartist-button');
+Object.values(chartistButtons).forEach(chartistButton => {
+  chartistButton.addEventListener('click', (event) => {
+    const key = event.target.dataset.key;
+    if (bouboumData !== null) {
+      drawChart(bouboumData, key);
     }
-    console.log(chartPoints)
-    new Chartist.Line('.ct-chart', data, options);
+  });
 });
