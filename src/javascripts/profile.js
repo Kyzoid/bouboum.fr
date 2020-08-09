62b585549eb1e7963646fba9b1163bbabf46f7ad
Kyzoid <kyzoid@gmail.com>
2020-08-09 20:20:41 +0200

    Add graph selection buttons

--- src/javascripts/profile.js
+++ src/javascripts/profile.js
@@ -10,62 +10,96 @@ const getAaaahRankings = async () => {
 
 const formattedBouboumPlayersDataForChartist = (bouboumRankings) => {
   return {
-    mode: 'Bouboum',
-    win: bouboumRankings.map(bouboumRanking => bouboumRanking.win),
-    ratio: bouboumRankings.map(bouboumRanking => bouboumRanking.ratio),
-    total: bouboumRankings.map(bouboumRanking => bouboumRanking.total),
-    winRatio: bouboumRankings.map(bouboumRanking => ((bouboumRanking.win/bouboumRanking.total)*100).toFixed(2)),
+    'bouboum-rank': [23, 32, 32, 30, 39, 29, 38, 19, 19, 23, 13, 32, 32, 39, 29, 32, 19, 19],
+    'bouboum-win': bouboumRankings.map(bouboumRanking => bouboumRanking.win),
+    'bouboum-ratio': bouboumRankings.map(bouboumRanking => bouboumRanking.ratio/100),
+    'bouboum-total': bouboumRankings.map(bouboumRanking => bouboumRanking.total),
+    'bouboum-win-ratio': bouboumRankings.map(bouboumRanking => ((bouboumRanking.win / bouboumRanking.total) * 100).toFixed(2)),
   }
 };
 
 const formattedAaaahPlayersDataForChartist = (aaaahRankings) => {
   return {
-    mode: 'Aaaah',
-    win: aaaahRankings.map(aaaahRanking => aaaahRanking.win),
-    winRatio: aaaahRankings.map(aaaahRanking => aaaahRanking.win_ratio/100),
-    guiding: aaaahRankings.map(aaaahRanking => aaaahRanking.guiding),
-    kill: aaaahRankings.map(aaaahRanking => aaaahRanking.kill),
-    guidingRatio: aaaahRankings.map(aaaahRanking => aaaahRanking.guiding_ratio),
-    total: aaaahRankings.map(aaaahRanking => aaaahRanking.total)
+    'aaaah-rank': [23, 32, 32, 30, 39, 29, 38, 19, 19, 23, 13, 32, 32, 39, 29, 32, 19, 19],
+    'aaaah-win': aaaahRankings.map(aaaahRanking => aaaahRanking.win),
+    'aaaah-win-ratio': aaaahRankings.map(aaaahRanking => aaaahRanking.win_ratio/100),
+    'aaaah-guiding': aaaahRankings.map(aaaahRanking => aaaahRanking.guiding),
+    'aaaah-kill': aaaahRankings.map(aaaahRanking => aaaahRanking.kill),
+    'aaaah-guiding-ratio': aaaahRankings.map(aaaahRanking => aaaahRanking.guiding_ratio/100),
+    'aaaah-total': aaaahRankings.map(aaaahRanking => aaaahRanking.total)
   }
 };
 
-const drawChart = (rankings, key) => {
-  const data = {
+const chartistData = (data) => {
+  return {
     labels: [],
     series: [
-      rankings[key],
+      {
+        name: 'series-1',
+        data: data,
+      }
     ]
   };
+};
 
-  const options = {
-    width: 700,
-    height: 300
-  };
-
-  new Chartist.Line('.ct-chart', data, options);
+const options = {
+  width: 700,
+  height: 300,
+  series: {
+    'series-1': {
+      lineSmooth: Chartist.Interpolation.simple()
+    }
+  }
 };
 
 let bouboumData = null;
 let aaaahData = null;
 
-getBouboumRankings().then(async (bouboumRankings) => {
-  if (bouboumRankings.length) {
-    bouboumData = formattedBouboumPlayersDataForChartist(bouboumRankings);
-    drawChart(formattedBouboumPlayersDataForChartist(bouboumRankings), 'win');
+const drawCharts = (gameData, charts, game) => {
+  Object.values(gameData).forEach((data, i) => {
+    const className = Object.keys(gameData)[i];
+    if (document.querySelector(`.${className}`) === null) {
+      const div = document.createElement('div');
+      div.classList.add('absolute');
+      div.classList.add('hidden');
+      div.classList.add(className);
+      charts.appendChild(div);
+    }
+    new Chartist.Line(`.${className}`, chartistData(data), options);
+  });
+};
+
+(async () => {
+  const bouboumRankings = await getBouboumRankings();
+  bouboumData = formattedBouboumPlayersDataForChartist(bouboumRankings)
+  const aaaahRankings = await getAaaahRankings();
+  aaaahData = formattedAaaahPlayersDataForChartist(aaaahRankings);
+
+  const charts = document.getElementById('charts');
+  drawCharts(bouboumData, charts, 'bouboum');
+  drawCharts(aaaahData, charts, 'aaaah');
+
+  if (bouboumRankings.length > 0) {
+    document.querySelector('.bouboum-rank').classList.remove('hidden');
   } else {
-    const aaaahRankings = await getAaaahRankings();
-    aaaahData = formattedAaaahPlayersDataForChartist(aaaahRankings);
-    drawChart(formattedAaaahPlayersDataForChartist(aaaahRankings), 'win');
+    document.querySelector('.aaaah-rank').classList.remove('hidden');
   }
-});
+})();
+
+const showChart = (event) => {
+  const key = event.target.dataset.key;
+  const allCharts = document.getElementById('charts').childNodes;
+  
+  allCharts.forEach(chart => {
+    if (!chart.classList.contains('hidden')) {
+      chart.classList.add('hidden');
+    }
+  });
+
+  document.querySelector(`.${key}`).classList.remove('hidden');
+};
 
 const chartistButtons = document.getElementsByClassName('chartist-button');
 Object.values(chartistButtons).forEach(chartistButton => {
-  chartistButton.addEventListener('click', (event) => {
-    const key = event.target.dataset.key;
-    if (bouboumData !== null) {
-      drawChart(bouboumData, key);
-    }
-  });
+  chartistButton.addEventListener('click', showChart);
 });
